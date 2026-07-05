32b1c798d77b57529269a7c188c3db717cfcffd5
SharvariLonkar <sharvarilonkar@gmail.com>
2026-07-05 18:41:26 -0400

    Did program more efficiently

--- program2.py
+++ program2.py
@@ -1,7 +1,11 @@
 # Practice Problem: Write a program to display numbers from -10 to -1 using a for loop.
 
-num = -10
+# num = -10
 
-for i in range(10):
-    print(num)
-    num += 1
+# for i in range(10):
+#     print(num)
+#     num += 1
+
+# used one variable instead of two and also the program became efficient because only one variable was being incremented in the for loop
+for i in range(-10,-1):
+    print(i)
