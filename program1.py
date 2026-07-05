96c457afa14e7c5a3317556af6744584876000d6
SharvariLonkar <sharvarilonkar@gmail.com>
2026-07-05 18:15:12 -0400

    finished first program

---
+++ program1.py
@@ -0,0 +1,8 @@
+#Practice Problem: Write a program to print the first 10 natural numbers using a while loop. 
+# Each number should be printed on a new line.
+
+num = 1
+
+while num <= 10:
+    print(num, "\n")
+    num += 1
