37b1bf575bc8be9e86f774ee0a1ebf5200b9c5b7
SharvariLonkar <sharvarilonkar@gmail.com>
2026-07-05 18:53:57 -0400

    did program 4 successfully

---
+++ program4.py
@@ -0,0 +1,14 @@
+# Practice Problem: Write a program that accepts a number from the user and 
+# calculates the sum of all numbers from 1 up to that number.
+
+try:
+    num = int(input("Provide a number:"))
+except ValueError:
+    print('Provide valid number')
+
+sum = 0
+
+for i in range(1, num+1):
+    sum += i
+
+print(f"Sum of all numbers from 1 to {num} is {sum}")
